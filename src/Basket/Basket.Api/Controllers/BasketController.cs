71ad57b454cb21aef4114bdcba10567837e09bd4
Luis Mazariego <mazariego2011@gmail.com>
2020-11-25 22:05:41 -0600

    Basket API

---
+++ src/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+    }
+}
---
+++ src/Basket/Basket.Api/Entities/BasketCart.cs
@@ -0,0 +1,38 @@
+﻿namespace Basket.Api.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class BasketCart
+    {
+        public string UserName { get; set; }
+        public List<BasketCartItem> Items { get; set; } = new List<BasketCartItem>();
+
+        public BasketCart()
+        {
+        }
+
+        public BasketCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        //calculate total price basket Price
+        public decimal TotalPrice 
+        {
+            get 
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            } 
+             
+        }
+
+    }
+}
